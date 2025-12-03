237e2f568e59e9ab41c9e299afc9839d15eab6dc
temdawud-tech <temdawud@gmail.com>
2025-12-03 23:39:26 +0300

    Update Palindrome Number.py

--- Palindrome Number.py
+++ Palindrome Number.py
@@ -1,2 +1,8 @@
-class Solution:
-    def isPalindrome(self, x: int) -> bool:
+class Solution(object):
+    def isPalindrome(self, x):
+        """
+        :type x: int
+        :rtype: bool
+        """
+        y = str(x)
+        return y[::] == y[::-1]
